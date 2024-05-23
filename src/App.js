5f2d8918d0615704f9eb7b56f8f268b302f26ea0
KieVitz <s217442@sggw.edu.pl>
2024-05-23 15:57:49 +0200

    Create basic page layout and the card component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Cards from "./components/Cards.js";
+import { ReactComponent as ReactIcon } from "./components/icons/react.svg";
+import { ReactComponent as JSIcon } from "./components/icons/js.svg";
+import { ReactComponent as P5Icon } from "./components/icons/p5.svg";
 
-function App() {
+import gameOfLifeImage from "./components/images/game-of-life.png";
+import fallingSandImage from "./components/images/falling-sand.png";
+import fourierSeriesImage from "./components/images/fourier-series.png";
+import rayTracingImage from "./components/images/raytracing.png";
+import rtRenderingImage from "./components/images/rt-rendering.png";
+import raymarchingImage from "./components/images/raymarching.png";
+
+const p5Projects = {
+  gameOfLife: {
+    title: "Game of Life",
+    description:
+      "The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970.",
+    image: gameOfLifeImage,
+    link: "https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life",
+    icons: [P5Icon, JSIcon],
+  },
+  fallingSand: {
+    title: "Falling Sand",
+    description:
+      "A falling sand simulation. Drag the mouse to draw elements and watch them interact. Press Shift to remove the sand.",
+    image: fallingSandImage,
+    link: "https://example.com",
+    icons: [P5Icon, JSIcon],
+  },
+  fourierSeries: {
+    title: "Fourier Series",
+    description:
+      "A Fourier series is an expansion of a periodic function into a sum of trigonometric functions. The Fourier series is an example of a trigonometric series.",
+    image: fourierSeriesImage,
+    link: "https://example.com",
+    icons: [P5Icon, JSIcon],
+  },
+  rayTracing: {
+    title: "2D Raytracing",
+    description:
+      "Ray tracing is a computer graphics technique used to generate highly realistic images by simulating the way light interacts with objects.",
+    image: rayTracingImage,
+    link: "https://example.com",
+    icons: [P5Icon, JSIcon],
+  },
+  rtRender: {
+    title: "2D RT - Rendering",
+    description: "Description of yet another project",
+    image: rtRenderingImage,
+    link: "https://example.com",
+    icons: [P5Icon, JSIcon],
+  },
+  rayMarching: {
+    title: "Raymarching",
+    description:
+      "Ray marching is a method where rays are traversed iteratively, effectively dividing each ray into smaller ray segments, sampling some function at each step.",
+    image: raymarchingImage,
+    link: "https://example.com",
+    icons: [P5Icon, JSIcon],
+  },
+};
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main>
+        <section className="blue">
+          <header>
+            <h1>GitHub Projects</h1>
+            <p>A collection of small projects built using the p5 library.</p>
+          </header>
+          <Cards content={p5Projects} iconsClass="blue" />
+        </section>
+        <section className="dark">
+          <header>
+            <h1>Kaggle Projects</h1>
+            <p>A collection of small projects built using the p5 library.</p>
+          </header>
+          <Cards content={p5Projects} iconsClass="dark" />
+        </section>
+        <section className="red">
+          <header>
+            <h1>JavaScript mini-projects</h1>
+            <p>A collection of small projects built using the p5 library.</p>
+          </header>
+          <Cards content={p5Projects} iconsClass="red" />
+        </section>
+      </main>
+      <footer>
+        <p>Created by Stanisław Stankiewicz</p>
+        <P5Icon style={{ width: "20px", height: "20px" }} />
+      </footer>
+    </>
   );
 }
-
-export default App;
---
+++ src/components/Cards.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./cards.css";
+import { ReactComponent as JSIcon } from "./icons/js.svg";
+
+export default function Cards({ content, iconsClass }) {
+  return (
+    <div className="cards">
+      {Object.values(content).map((project) => (
+        <Card key={project.title} content={project} iconsClass={iconsClass} />
+      ))}
+    </div>
+  );
+}
+
+function Card({ content, iconsClass }) {
+  const { title, description, image, link, icons } = content;
+  return (
+    <div className="card">
+      <div className="card-image">
+        <img src={image} alt="Avatar" />
+      </div>
+      <div className={`card-icons ${iconsClass}`}>
+        {icons.map((Icon, index) => (
+          <Icon key={index} className={`card-icon`} />
+        ))}
+      </div>
+      <div className="card-header">
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
