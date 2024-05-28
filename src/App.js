5603a4dd0474250775fb68fbbf38b966152e732f
KieVitz <s217442@sggw.edu.pl>
2024-05-28 19:10:46 +0200

    Add header interactivity and extract sections into a function

--- src/App.js
+++ src/App.js
@@ -1,52 +1,67 @@
 import "./App.css";
 import Cards from "./components/Cards.js";
+import Header from "./components/Header.js";
 import { p5Projects, githubProjects } from "./projects.js";
 
 export default function App() {
   return (
     <>
-      <header className="main-header">
-        <nav>
-          <p>About me</p>
-          <p>GitHub</p>
-          <p>LinkedIn</p>
-          <p>Kaggle</p>
-          <p>JavaScript</p>
-        </nav>
-      </header>
+      <Header
+        className="main-header"
+        sections={["GitHub", "Kaggle", "JavaScript"]}
+      />
       <main>
-        <section className="blue" style={{ paddingTop: "30px" }}>
-          <header className="sub-header">
-            <h1>GitHub Projects</h1>
-            <p>
-              These are all projects I did as a practice or because I felt a
-              need to.
-            </p>
-          </header>
-          <Cards content={githubProjects} iconsClass="blue" />
-          <div className="spacer layer1"></div>
-        </section>
+        <Section
+          id="github"
+          title="GitHub Projects"
+          description="These are all projects I did as a practice or because I felt a need to."
+          projects={githubProjects}
+          backgroundClass="blue"
+          spacerClass="spacer layer1"
+          style={{ paddingTop: "100px" }}
+        />
 
-        <section className="dark">
-          <header className="sub-header">
-            <h1>Kaggle Projects</h1>
-            <p>All machine learning projects I did.</p>
-          </header>
-          <Cards content={p5Projects} iconsClass="dark" />
-          <div className="spacer layer2"></div>
-        </section>
+        <Section
+          id="kaggle"
+          title="Kaggle Projects"
+          description="All machine learning projects I did."
+          projects={p5Projects}
+          backgroundClass="dark"
+          spacerClass="spacer layer2"
+        />
 
-        <section className="red">
-          <header className="sub-header">
-            <h1>JavaScript mini-projects</h1>
-            <p>A collection of small projects built using the p5 library.</p>
-          </header>
-          <Cards content={p5Projects} iconsClass="red" />
-        </section>
+        <Section
+          id="javascript"
+          title="JavaScript mini-projects"
+          description="A collection of small projects built using the p5 library."
+          projects={p5Projects}
+          backgroundClass="red"
+        />
       </main>
       <footer>
         <p>Created by Stanisław Stankiewicz</p>
       </footer>
     </>
+  );
+}
+
+function Section({
+  id,
+  title,
+  description,
+  projects,
+  backgroundClass,
+  spacerClass,
+  style = {},
+}) {
+  return (
+    <section id={id} className={backgroundClass} style={style}>
+      <header className="sub-header">
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </header>
+      <Cards content={projects} iconsClass={backgroundClass} />
+      <div className={`spacer ${spacerClass}`}></div>
+    </section>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from "react";
+import "./header.css";
+
+export default function Header({ children, className, sections = [] }) {
+  const [scrolled, setScrolled] = useState(false);
+  const [selectedId, setSelectedId] = useState(
+    sections[0].toLowerCase().replace(/\s+/g, "")
+  );
+
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if (offset > 70) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+
+    // determine which section is currently active
+    const sectionIds = sections.map((section) =>
+      section.toLowerCase().replace(/\s+/g, "")
+    );
+
+    const X = 400; // Change this to the desired value
+
+    for (let id of sectionIds) {
+      const element = document.getElementById(id);
+      if (element) {
+        const rect = element.getBoundingClientRect();
+        const top = rect.top + window.scrollY - X;
+        const bottom = top + rect.height;
+
+        if (offset >= top && offset < bottom) {
+          setSelectedId(id);
+          break;
+        }
+      }
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  });
+
+  return (
+    <header
+      className={className || ""}
+      style={
+        scrolled
+          ? {
+              backgroundColor: "transparent",
+              backdropFilter: "blur(30px)",
+            }
+          : {}
+      }
+    >
+      <ul className="sections">
+        {sections.map((section) => (
+          <li
+            key={section}
+            onClick={() => {
+              const id = section.toLowerCase().replace(/\s+/g, "");
+              const element = document.getElementById(id);
+              if (element) {
+                const offset = 100;
+                const bodyRect = document.body.getBoundingClientRect().top;
+                const elementRect = element.getBoundingClientRect().top;
+                const elementPosition = elementRect - bodyRect;
+                const offsetPosition = elementPosition - offset;
+
+                window.scrollTo({
+                  top: offsetPosition,
+                  behavior: "smooth",
+                });
+              }
+            }}
+          >
+            <p
+              className={
+                selectedId === section.toLowerCase().replace(/\s+/g, "")
+                  ? "selected"
+                  : ""
+              }
+            >
+              {section}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </header>
+  );
+}
